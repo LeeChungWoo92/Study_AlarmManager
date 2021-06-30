5e5087760b5f1e915cc9197c78476b74945023ea
LeeChungWoo <skcjddn@gmail.com>
2021-06-30 23:58:08 +0900

    add TimePickDialog

--- app/src/main/java/com/lcw/study/alarmmanager/MainActivity.kt
+++ app/src/main/java/com/lcw/study/alarmmanager/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.lcw.study.alarmmanager
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initOnOffButton()
+        initChangeAlarmTimeButton()
+    }
+
+    private fun initOnOffButton() {
+        val onOffButton = findViewById<Button>(R.id.onOffButton)
+        onOffButton.setOnClickListener {
+            // 데이터 확인
+            // 온오프에 따라 작업을 처리한다.
+
+            // 오프 -> 알람을 제거
+            // 온 -> 알람을 등록
+
+            //데이터를 저장한다.
+        }
+    }
+
+    private fun initChangeAlarmTimeButton() {
+        val changeAlarmButton = findViewById<Button>(R.id.changeAlarmTimeButton)
+        changeAlarmButton.setOnClickListener {
+            // TimePickDialog 띄워저서 시간을 설정을 하도록 하게끔 하고, 그시간을 가져와서
+
+            val calendar = Calendar.getInstance()
+            // 데이터를 저장한다.
+            // 뷰를 업데이트한다
+            // 기존에 있던 알람을 삭제한다
+        }
+
     }
 }
